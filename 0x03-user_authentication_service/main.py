9c9152536893f310d8a58bcafb5cd29fb0cef9ea
James Alhassan <jamesalhassan052@gmail.com>
2023-08-18 05:36:32 +0000

    Update main.py

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -113,3 +113,16 @@ def update_password(email: str, reset_token: str, new_password: str) -> None:
 
     assert response.status_code == 200
     assert response.json() == msg
+
+
+if __name__ == "__main__":
+
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
