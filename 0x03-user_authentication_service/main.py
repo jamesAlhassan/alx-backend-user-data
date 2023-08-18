05da497b2749375d33732274899f89591c4418de
James Alhassan <jamesalhassan052@gmail.com>
2023-08-18 05:29:36 +0000

    Add main.py

---
+++ 0x03-user_authentication_service/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" End-to-end integration test"""
+
+import requests
+
+BASE_URL = 'http://localhost:5000'
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+
+
+def register_user(email: str, password: str) -> None:
+    """ Test for validating user registration """
+    data = {
+        "email": email,
+        "password": password
+    }
+    response = requests.post(f'{BASE_URL}/users', data=data)
+
+    msg = {"email": email, "message": "user created"}
+
+    assert response.status_code == 200
+    assert response.json() == msg
